e990f08b24b0aa657da1fcd541b3dd9fd47e0aa8
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-01-07 16:32:41 +0530

    changes

--- day_1/day_1_code.py
+++ day_1/day_1_code.py
@@ -1,2 +1,16 @@
 print(3+4)
 print(2+1)
+print("Hello World")
+print("Anamika Upadhyay")
+print("Anamika is my name.", "My age is 17")
+print(12*2)
+name = "Anamika"
+age = 17
+price = 12
+
+age2 = age
+
+print("my name is : ", name)
+print("my age is :", age)
+print(age2)
+
