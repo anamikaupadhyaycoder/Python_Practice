893c1186dd4744d0cc4f07084e74ec7a4aacdc63
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-01-16 10:07:47 +0530

    update changes

--- string.py
+++ string.py
@@ -1,68 +1,68 @@
-#arithmetic operators
+arithmetic operators
 a = 5
 b = 2 
 
 
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a % b) #reminder
-# print(a ** b) #a^b 
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #reminder
+print(a ** b) #a^b 
 
-#relational / comparison operators 
+relational / comparison operators 
 a = 50
 b = 20
 
-# print(a == b) #False
-# print(a != b) #True
-# print(a > b) #True
-# print(a < b) #False
-# print(a >= b) #True
-# print(a <= b) #False
+print(a == b) #False
+print(a != b) #True
+print(a > b) #True
+print(a < b) #False
+print(a >= b) #True
+print(a <= b) #False
 
 
-# assigment operator 
-# num = 10 
-# num += 10
-# print(num) #20
-# num -= 10
-# print(num) #10
-# num *=10
-# print(num) #100
+assigment operator 
+num = 10 
+num += 10
+print(num) #20
+num -= 10
+print(num) #10
+num *=10
+print(num) #100
 
-# num = 10
-# num **=5
-# print(num) #100,000
+num = 10
+num **=5
+print(num) #100,000
 
-# num = 14
-# num %=5
-# print(num) #2
-# num /= 10 
-# print(num)
-
-
-#Logical operators
-# a = 50 #not
-# b = 30
-
-# print(not (a < b))
-# print(not (a > b))
-
-# Val1 = True #and
-# Val2 = True
-# print("and operator:", (a == b) and (a > b))
-
-# Val1 = True #or
-# Val2 = False
-# print("or operator:", (a == b) or (a > b))
+num = 14
+num %=5
+print(num) #2
+num /= 10 
+print(num)
 
 
-#type conversion
-# a = float("2")
-# b = 4.25
-# print(type(a))
-# print(a + b) 
+Logical operators
+a = 50 #not
+b = 30
+
+print(not (a < b))
+print(not (a > b))
+
+Val1 = True #and
+Val2 = True
+print("and operator:", (a == b) and (a > b))
+
+Val1 = True #or
+Val2 = False
+print("or operator:", (a == b) or (a > b))
+
+
+type conversion
+a = float("2")
+b = 4.25
+print(type(a))
+print(a + b) 
 a = 3.14 
 a = str("3.14")
 print(type(a))
