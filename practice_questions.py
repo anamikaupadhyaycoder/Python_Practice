54a5ca3b8f0ace464834abfc7985729571ae4926
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-01-17 19:08:53 +0530

    added new codes

--- practice_questions.py
+++ practice_questions.py
@@ -1,4 +1,6 @@
-a = int(input("first number: "))
-b = int(input("second number: "))
+# a = int(input("first number: "))
+# b = int(input("second number: "))
 
-print(a >= b)
+# print(a >= b)
+
+print("Anshika")
