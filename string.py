b69acf994174421b487677c7a739b10b0feb3478
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-02-05 13:18:22 +0530

    update changes

--- string.py
+++ string.py
@@ -1,68 +1,76 @@
-arithmetic operators
-a = 5
-b = 2 
+# arithmetic operators
+# a = 5
+# b = 2 
 
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a % b) #reminder
-print(a ** b) #a^b 
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b) #reminder
+# print(a ** b) #a^b 
 
-relational / comparison operators 
-a = 50
-b = 20
+# relational / comparison operators 
+# a = 50
+# b = 20
 
-print(a == b) #False
-print(a != b) #True
-print(a > b) #True
-print(a < b) #False
-print(a >= b) #True
-print(a <= b) #False
-
-
-assigment operator 
-num = 10 
-num += 10
-print(num) #20
-num -= 10
-print(num) #10
-num *=10
-print(num) #100
-
-num = 10
-num **=5
-print(num) #100,000
-
-num = 14
-num %=5
-print(num) #2
-num /= 10 
-print(num)
+# print(a == b) #False
+# print(a != b) #True
+# print(a > b) #True
+# print(a < b) #False
+# print(a >= b) #True
+# print(a <= b) #False
 
 
-Logical operators
-a = 50 #not
-b = 30
+# assigment operator 
+# num = 10 
+# num += 10
+# print(num) #20
+# num -= 10
+# print(num) #10
+# num *=10
+# print(num) #100
 
-print(not (a < b))
-print(not (a > b))
+# num = 10
+# num **=5
+# print(num) #100,000
 
-Val1 = True #and
-Val2 = True
-print("and operator:", (a == b) and (a > b))
-
-Val1 = True #or
-Val2 = False
-print("or operator:", (a == b) or (a > b))
+# num = 14
+# num %=5
+# print(num) #2
+# num /= 10 
+# print(num)
 
 
-type conversion
-a = float("2")
-b = 4.25
-print(type(a))
-print(a + b) 
-a = 3.14 
-a = str("3.14")
-print(type(a))
+# Logical operators
+# a = 50 #not
+# b = 30
+
+# print(not (a < b))
+# print(not (a > b))
+
+# Val1 = True #and
+# Val2 = True
+# print("and operator:", (a == b) and (a > b))
+
+# Val1 = True #or
+# Val2 = False
+# print("or operator:", (a == b) or (a > b))
+
+
+# type conversion
+# a = float("2")
+# b = 4.25
+# print(type(a))
+# print(a + b) 
+# a = 3.14 
+# a = str("3.14")
+# print(type(a))
+
+# str1 = "hello"
+# str2 = "world"
+# print(str1+str2)
+# print(len(str2))
+a  = int(input("weight: "))
+if a > 25: 
+  print("warning: luggage exceeds the allowed limit of 25 kg.")
