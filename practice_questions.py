10cbcb16e36cadc0228a6813b2829b16eaaf920f
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-03-04 18:05:56 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,9 +1,51 @@
 #WAP to input user's first name & print its length
 
-a = input("enter your name : ")
-print("length of your name is", len(a))
+# a = input("enter your name : ")
+# print("length of your name is", len(a))
 
-#WAP to find the occurrence of '$' in a string.
+# #WAP to find the occurrence of '$' in a string.
 
-name = "this is a $ symbol of dollar and $ its used in usa most"
-print(name.count("$"))
+# name = "this is a $ symbol of dollar and $ its used in usa most"
+# print(name.count("$"))
+
+#WAP to give grade students based on marks 
+
+# marks = 75
+# if(marks >= 90):
+#     print("A")
+# elif(90 > marks >= 80):
+#     print("B")
+# elif(80 > marks >= 70      ):
+#     print("C")
+# else:
+#     print("D")
+
+#WAP to check if a number entered by the user is odd or even.
+
+# num = int(input("enter the number : "))
+# if (num % 2 == 0):
+#     print("even")
+# elif(num % 2 != 0):
+#     print("odd")
+
+#WAP to find the greatest of 3 numbers entered by the user.
+
+# a = 10
+# b = 15
+# c = 14
+
+# if(a > b and a > c):
+#     print("a is greatest")
+# elif(b > a and b > c):
+#     print("b is greatest")
+# else:
+#     print("c is greatest")
+
+#WAP to check if a number is a multiple of 7 or not.
+
+num = int(input("enter the number : "))
+if(num % 7 == 0):
+    print("multiple of 7 is true")
+else:
+    print("multiple of 7 is false")   
+    
