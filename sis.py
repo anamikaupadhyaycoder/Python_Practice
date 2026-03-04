20cfdd4e94c260fb154b653b726f5d35ee7e590e
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-03-04 16:39:39 +0530

    update changes

--- sis.py
+++ sis.py
@@ -1,3 +1,17 @@
 A = int(input("weight of luggaage : "))
 if(A > 25): 
     print("warning, luggage over exceed")
+
+light = "green"
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("wait")
+
+n = 6
+if(n > 4): 
+    print("greater than 4")
+elif(n > 5):
+    print("greater than 5")
