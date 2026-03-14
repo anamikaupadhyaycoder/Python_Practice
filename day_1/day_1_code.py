64debaab43ab10ba8c1a718a69f4377ffefd8283
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-03-14 19:29:07 +0530

    update changes

--- day_1/day_1_code.py
+++ day_1/day_1_code.py
@@ -1,15 +1,2 @@
-lists = [3, 5, 6, 3, 2, 6, 5, 9]
-lists.append(7)
-print(lists)
-lists.sort(reverse = True)
-print(lists)
-
-#we can also perform this functions within string.
-
-list = ["a", "b", "d", "w"]
-list.append("g")
-
- 
-print(list)
-list.insert(2, "s")
-print(list)
+tuple = (2, 5, 3, 6, 6, 3, 2,)
+print(tuple.count(2))
