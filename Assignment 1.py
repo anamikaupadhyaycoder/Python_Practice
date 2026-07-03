e874f081498635ff9acfc0021783881eed374a4b
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-03 22:22:51 +0530

    update changes

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,3 +1,7 @@
-# Assigment questioin : print your name 10 times using * symbol
-
-print(("Aayushi " + "\n") * 10)
+#question - Take diameter as input and calculate area of a circle
+diameter_input = input("enter diameter of circle: ", )
+diameter = float(diameter_input)
+radius = diameter/2
+pi = 3.14
+area = pi*(radius ** 2)
+print("the  area of the circle is : ", area)
