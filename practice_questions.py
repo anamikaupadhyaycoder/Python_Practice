114016948677260869d972fda648d63f7545e204
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-05 20:03:41 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,82 +1,13 @@
-#WAP to input user's first name & print its length
+# question
+new_dict = {
+    1 : 1**3,
+    2 : 2**3,
+    3 : 3**3,
+    4 : 4**3,
+    5 : 5**3
+}
+print(new_dict)
 
-# a = input("enter your name : ")
-# print("length of your name is", len(a))
-
-# #WAP to find the occurrence of '$' in a string.
-
-# name = "this is a $ symbol of dollar and $ its used in usa most"
-# print(name.count("$"))
-
-#WAP to give grade students based on marks 
-
-# marks = 75
-# if(marks >= 90):
-#     print("A")
-# elif(90 > marks >= 80):
-#     print("B")
-# elif(80 > marks >= 70      ):
-#     print("C")
-# else:
-#     print("D")
-
-#WAP to check if a number entered by the user is odd or even.
-
-# num = int(input("enter the number : "))
-# if (num % 2 == 0):
-#     print("even")
-# elif(num % 2 != 0):
-#     print("odd")
-
-#WAP to find the greatest of 3 numbers entered by the user.
-
-# a = 10
-# b = 15
-# c = 14
-
-# if(a > b and a > c):
-#     print("a is greatest")
-# elif(b > a and b > c):
-#     print("b is greatest")
-# else:
-#     print("c is greatest")
-
-#WAP to check if a number is a multiple of 7 or not.
-
-# num = int(input("enter the number : "))
-# if(num % 7 == 0):
-#     print("multiple of 7 is true")
-# else:
-#     print("multiple of 7 is false")   
-
-
-#WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
-
-# a = input("enter first movie name: ")
-# b = input("enter second movie name: ")
-# c = input("enter third movie name: ")
- 
-# list = [a, b, c]
-# print(list)
-
-#WAP to check if  list contains a palindrome of elements. 
-
-# list1 = [1, 2, 1]
-# list2 = [2,3,4]
-
-# copy_list1 = list1.copy()
-# copy_list1.reverse() 
-
-# if(copy_list1 == list1):
-#     print("palindrome")
-# else:
-#     print("non palindrome")
-
-#WAP to count the number of students with the "A" grade in the following tuple.
-
-tup = ["A", "B", "A", "C", "B","C", "A", "D", "C"]
-a = tup.sort()
-print(tup)
 
 
 
