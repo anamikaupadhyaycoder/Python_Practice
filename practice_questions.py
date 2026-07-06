41e86f36476373314bf987abdd5397e3e7c527c7
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-06 21:02:16 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,8 +1,21 @@
-# Assignment question
-python_dict = {
-    "table" : ["a piece of furniture", "list of facts & figures"],
-    "cat" : "a small animal"
-}
-print(python_dict)
+empty_dict = {}
+sub_1 = input("enter the marks of sub_1 : ")
+sub_2 = input("enter the marks of sub_2 : ")
+sub_3 = input("enter the marks of sub_3 : ")
+
+empty_dict.update({"phy" : sub_1})
+empty_dict.update({"maths" : sub_2})
+empty_dict.update({"bio" : sub_3})
+
+print(empty_dict)
+
+set_4 = {9, "9.0"}
+print(set_4)
+
+set_1 = {("float", 9.0), ("int", 9)}
+print(set_1)
+
+
+
 
 
