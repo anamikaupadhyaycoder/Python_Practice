6b2aa139afc40df47b28ccde9059302c483a3443
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-09 20:19:58 +0530

    update changes

--- loops.py
+++ loops.py
@@ -1,6 +1,35 @@
 # question 
-# print numbers from 1 to 100
-i = 100
-while i >= 1:
-    print(i)
-    i-=1
+# print multiple of 4 
+'''i = 1
+while i<= 10:
+    print(4*i)
+    i+=1'''
+    
+#nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# print the elements of the folowing list using a loop
+
+tu = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+x = 49
+
+i = 0# initalization
+while i<len(tu):
+    if(tu[i] == x):
+     print("found at idx:", i)
+     break
+    else:
+        print("finding...")
+    i+=1
+
+list = [1,2,3]
+
+for el in list:
+    print(el)
+
+
+
+
+
+
+
+
