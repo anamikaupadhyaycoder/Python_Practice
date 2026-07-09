cd673d084b1be549be275bde0d59f44122c548ee
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-09 20:20:34 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,19 +1,29 @@
-empty_dict = {}
-sub_1 = input("enter the marks of sub_1 : ")
-sub_2 = input("enter the marks of sub_2 : ")
-sub_3 = input("enter the marks of sub_3 : ")
+# smart temperature converter 
+''' take input in celsius and print its equivalent in fahrenheit and kelvin.
+(use explicit type conversion and arithmetic operators.)
+ fahreneheit = (c x 9/5) + 32
+ kelvin = c + 273.15'''
 
-empty_dict.update({"phy" : sub_1})
-empty_dict.update({"maths" : sub_2})
-empty_dict.update({"bio" : sub_3})
+cel = int(input("enter the value of celsius :"))
+fah = (cel * 9/5) + 32
+kel = cel + 273.15
 
-print(empty_dict)
+print("equivalent in fahrenheit :", fah)
+print("equivalent in kelvin :", kel)
 
-set_4 = {9, "9.0"}
-print(set_4)
+# write a program that takes total bill amount and number of friends as input. 
+# calculate how much each person will pay
+# Also print the data type of each variable used.
 
-set_1 = {("float", 9.0), ("int", 9)}
-print(set_1)
+fri = float(input("enter no of friends: "))
+to_amo = float(input("enter total amount: "))
+
+each = to_amo/fri
+print("each person will pay: ", each)
+print(type(fri))
+print(type(to_amo))
+print(type(each))
+
 
 
 
