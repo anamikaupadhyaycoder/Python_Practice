207be6d61ddce62ed9bf99ba5bb8eebdd071da63
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-09 20:22:37 +0530

    update changes

---
+++ Assignment.py
@@ -0,0 +1,14 @@
+# question Write a python program that takes your age as input as prints
+#      The value Enterd
+#      Its data type 
+
+age =input("Enter the age : ")
+print(age)
+print(type(age))
+
+#question Input two number and print their sum.
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+sum = num1 + num2
+print(sum)
