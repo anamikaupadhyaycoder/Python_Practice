f573974542e51d5c94b4364356c80172c385fcef
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-09 21:06:31 +0530

    update changes

--- loops.py
+++ loops.py
@@ -1,31 +1,26 @@
-# question 
-# print multiple of 4 
-'''i = 1
-while i<= 10:
-    print(4*i)
-    i+=1'''
+# print the elements of the following list using a loop:
+'''li = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+for val in li:
+    print(val)
+else:
+    print("End of loop")'''
+
+#Search for a number x in this tupel using loop:
+
+tu = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 1)
+
+x = 1
+
+idx = 0
+
+for val in tu:
+    if(val == x):
+        print("number x is found at idx", idx)
+    idx += 1
+        
+        
     
-#nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-# print the elements of the folowing list using a loop
-
-tu = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
-
-x = 49
-
-i = 0# initalization
-while i<len(tu):
-    if(tu[i] == x):
-     print("found at idx:", i)
-     break
-    else:
-        print("finding...")
-    i+=1
-
-list = [1,2,3]
-
-for el in list:
-    print(el)
-
 
 
 
