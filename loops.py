92001bcf254d6b52327d77fe27245524a13a7e97
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-11 17:29:19 +0530

    update changes

--- loops.py
+++ loops.py
@@ -1,14 +1,4 @@
-# print the elements of the following list using a loop:
-'''li = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-
-for val in li:
-    print(val)
-else:
-    print("End of loop")'''
-
-#Search for a number x in this tupel using loop:
-
-tu = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 1)
+'''tu = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 1)
 
 x = 1
 
@@ -17,7 +7,41 @@ idx = 0
 for val in tu:
     if(val == x):
         print("number x is found at idx", idx)
-    idx += 1
+    idx += 1'''
+
+'''for i in range(20):# range(stop)
+    print(i)
+
+for i in range(10, 20):# range(start, stop) start is included and stop is not included
+    print(i)
+
+for i in range(10, 20, 2):# range(start, stop, step increment)
+    print(i)'''
+
+
+
+
+'''idx = 0
+
+for i in range(1, 101):
+    if(i == 36):
+        print(i)
+        idx += 1
+else:
+    print("i is found: ", idx)'''
+
+
+
+for i in range(5):
+    pass
+
+print("Hello World")
+
+
+
+    
+    
+
         
         
     
