c1dc2b6a506f7f5a42e2c79711158b1c8da7ea26
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-15 21:01:44 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,41 +1,26 @@
+# write a recursive function to calculate the sum of first n natural numbers.
 
-#WAF to print the length of a list. (list is the parameter)
-
-numbers = [1, 4, 3, 6, 8, 4, 9]
-names = ["anamika", "aayushi", "isha", "riva", "shreya"]
-
-def print_len(list):
-    print(len(list))
-
-print_len(numbers)
-print_len(names)
-
-#WAF to print the elements of a list in a single line.(list is the parameter)
-
-list1 = ["anamika", "aayushi", "isha", "riva", "shreya"]
- 
-def print_ele(list1):
-    print(list1[0:])
-
-print_ele(list1)
+'''def calc_sum(n):
+    if(n == 0):
+        return 0
+    return calc_sum(n-1) + n
 
 
-#WAF to find the factorial of n. (n is the parameter)
+sum = calc_sum(6)
+print(sum)'''
 
-def find_fac(n):
-    fact = 1
-    for i in range(1, n+1):
-        fact *= i 
-    print(fact)
-
-find_fac(5)
+# Write a recursive function to print all elements in a ist.
+# Hint: use list and index as parameters
 
 
-#WAF to convert USD to INR
+names = ["anamika", "aayushi", "isha", "shreya"]
 
-def conv(usd_val):
-    inr_val = usd_val*83
-    print(usd_val, "USD=", inr_val, "INR")
+def fun_list(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
 
+print(names)
+    
 
-conv(3)
