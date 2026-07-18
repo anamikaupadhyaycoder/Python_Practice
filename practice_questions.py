3e22a2c94957eb1ec0f4546d07eb3c061fa8dfb2
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-18 19:47:46 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,63 +1,67 @@
 # Basic Arithmetic & Operations(Q31-50)
-#1. Add,subtract,multiply,and divide two numbrs enterd by the user.
-num1 = int(input("Enter a number:"))
-num2 = int(input("Enter a number:"))
-sum = num1+num2
-subtract = num1-num2
-multiply = num1*num2
-divide = num1/num2
+#11. Round the decimal number 7.8945 to exactly two decimal places.
+x = 7.8945
+round_x = round(x, 2)
+print(round_x)
+
+#12. Calculate the absolute value of $-15$ using a built-in function.
+num = -15
+absolute_value = abs(num)
+print(f"original: num = {num}")
+print(f"Absolute value: absolut_value = {absolute_value}")
+
+#13. Find the square root of $144$ using the math module or exponentiation.
+x = 144
+squ_root = x**0.5
+print("square root: ",squ_root)
+
+#14. Compute the final price of an item after applying a $15\%$ discount.
+item = 1000 
+dis = 1000 * 15/100
+print(dis)
+
+#15. Ask for a 2-digit number and print the sum of its individual digits.
+two_dig = input("Ask for a two digit number:")
+fir_dig = int(two_dig[0])
+sec_dig = int(two_dig[1])
+sum = fir_dig + sec_dig
 print(sum)
-print(subtract)
-print(multiply)
-print(divide)
 
-#2. Find the remainder when $27$ is divided by $4$ using the modulo operator %.
-remainder = 27%4
-print(remainder)
+#16. Calculate Body Mass Index (BMI) using weight (kg) and height (meters).
+wei = 22
+hei = 5.5
+bmi = wei/hei**2
+print("Body mass index: ", bmi)
 
-#3. Calculate $3$ raised to the power of $4$ ($3^4$) using the exponentiation operator **.
-power = 3**4
-print(power)
+#17. Convert kilometers to miles ($1 \text{ km} \approx 0.621371 \text{ miles}$).
+km_1 = 0.621371
+n = 4# conversion of 4km into miles
+km_4 = n * km_1
+print(km_4)
 
-#4. Find the floor division quotient when $19$ is divided by $5$ using //.
-floor_division = 19//4
-print(floor_division)
+#18. Calculate the compound interest given principal, rate, and time.
+p = 100000
+r = 10/100
+n = 2#semi annual: interest is added every 6 months
+t = 1
+A = p*(1+(r/n))**(n*t)
+ci = A - p
+print("compound : ", ci)
 
-#5. Write a program to calculate the area of a rectangle ($length \times width$).
-length = int(input("enter length:"))
-breadth = int(input("enter breadth:"))
-area = length*breadth
-print(f"area of rectangle: {area}")
+#19. Find the perimeter of a square given its side length.
+len = 4
+side = 4*len
+print("perimeter of square:", side)
 
-#6. Calculate the area of a circle using radius input ($Area = \pi r^2$, use $3.14159$).
-radius = int(input("enter radius of circle:"))
+#20. Write a program that calculates the volume of a sphere ($V = \frac{4}{3} \pi r^3$).
+r = 2
 pie = 3.14
-area_of_circle = (radius**2)*pie
-print(f"area of circle: {area_of_circle}")
+volume = 4/3*pie*r**3
+print("volume of sphere:", volume)
 
-#7. Convert a temperature from Fahrenheit to Celsius ($C = \frac{F - 32}{1.8}$).
-Fahrenheit = int(input("enter value of fahrenheit:"))
-conv = (Fahrenheit - 32)/1.8
-print(conv)
 
-#8. Convert a given number of minutes into hours and remaining minutes.
-min = int(input("enter a minute:"))
-hou = min/60 
-print(hou)
 
-#9. Calculate Simple Interest: $SI = \frac{P \times R \times T}{100}$
-principle = int(input("enter the principle value:"))
-rate = int(input("enter the rate:"))
-time = int(input("enter the time:"))
-simple_Interest = principle*rate*time/100
-print(simple_Interest)
 
-#10. Ask the user for three numbers and print their mathematical average.
-num1 = int(input('enter number one:'))
-num2 = int(input('enter number two:'))
-num3 = int(input('enter number three:'))
-average = num1+num2+num3/3
- 
 
 
 
