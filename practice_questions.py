1a38356fdab344c2a404493a42d68fedd27afdc1
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-25 20:25:23 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,110 +1,82 @@
-# Topic 3: Lists and Tuples(50 questions)
-# List Operations (Q101–115)
-#1. Create a list of 5 movies and print the full list.
-movies = ["3 idiots", "pk", "narshima", "mardani", "mardani 2"]
-print(movies)
-print(type(movies))
-
-#2. Access and print the 3rd element of a list.
-num = [1, 2, 3, 4, 5 ]
-print(num[3])
-
-#3. Access the last element of a list using a negative index.
-num = [1, 2, 3, 4, 5 ]
-print(num[-1])
-
-#4. Change the first element of a list of numbers to $99$.
-lis = [1, 3, 4, 2, 6, 4 ,7]
-lis[0] = 99
-print(lis)
-
-#5. Use .append() to add a new city to a list of cities.
-cities = ["delhi", "mumbai", "hyderabad", "bangalore"]
-cities.append("punjab")
-print(cities)
-
-#6. Insert an element at index $1$ of an existing list using .insert().
-lsit = [3, 5, 2, 6, 7, 8, 0]
-lsit.insert(1, 3)
-print(lsit)
-
-#7. Remove an element from a list by its exact name using .remove().
-lis = [4, 7, 8, 3, 4, 1, 2]
-lis.remove(7)
-print(lis)
-
-#8. Remove and return the last element of a list using .pop().
-lis = [4, 6, 7, 3, 1, 9]
-last_lis = lis.pop()
-print(lis)
-print(last_lis)
-
-#9. Check if the value "Banana" exists in a list of fruits.
-fruits = ["mango", "banana", "apple", "litchi"]
-if fruits.count("banana") > 0:
-    print("Banana exits")
-else:
-    print("Banana doesn't exist")
-
-#10. Find the index position of a specific element in a list.
-lis = [2, 4, 5, 1, 6, 7, 9]
-print(lis.index(6))
-
-#11. Count how many times the number $5$ appears in a list.
-lis = [4, 2, 5, 8, 5, 0, 5, 1, 5]
-print(lis.count(5))
-
-#12. Find the total length (number of items) of a list.
-lsi = ["anamika", "aayushi", "isha", "shreya"]
-print(len(lsi))
-
-#13. Concatenate (combine) two lists together.
-lis1 = [1, 2, 3]
-lis2 = [4, 5, 6]
-lis = lis1+lis2
-print(lis)
-
-#14. Clear all elements from a list so it becomes completely empty.
-list1 = [5, 6, 3, 7, 2, 7 ]
-cl = list1.clear()
-print(list1)
-
-#15. Create a list of 5 elements and slice the middle 3 elements out.
-list1 = [5, 6, 3, 7, 2, 7 ]
+#List Slicing & Built-ins (Q116–130)
+#1.Print the first 3 elements of a list using slicing.
+list1 = [4, 2, 6, 8, 9, 0]
 print(list1[0:3])
 
+#2. Print every second element of a list using steps in slicing.
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+print(list1[::2])
 
+#3. Reverse a list using slicing ([::-1]).
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+print(list1[::-1])
 
+#4. Find the maximum value in a list of integers using max().
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+max = max(list1)
+print(max)
 
+#5. Find the minimum value in a list of integers using min().
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+min = min(list1)
+print(min)
 
+#6. Calculate the sum of all numbers in a numeric list using sum().
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+sum = sum(list1)
+print(sum)
 
+#7. Sort a list of numbers in ascending order using .sort().
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+list1.sort()
+print(list1)
 
+#8. Sort a list of numbers in descending order using sorted()
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+descending_order = sorted(list1, reverse=True)
 
+print("Original list:", list1)          # Output: 
+print("New sorted list:", descending_order) 
 
+#9. Duplicate a list's content three times using the multiplication operator *.
+list1 = [4, 2, 6, 8, 9, 0, 7, 8]
+d = list1*3
+print(d)
 
+#10. Split a list of 6 items into two equal halves.
+list1 = [4, 2, 6, 8, 9, 0]
+print(list1[0:3])
+print(list1[3:6])
 
+#11. Find the second largest number in a list of unique numbers.
+list1 = [4, 2, 6, 9, 0, 7, 8]
+list1.sort()
+print(list1)
+print(list1[-2])
 
+#12. Replace all negative numbers in a list with $0$.
+list1 = [4, -2, 6, -9, 0, 7, -8]
+list1 = [num if num >= 0 else 0 for num in list1]
 
+print(f"final list : {list1}")
 
+#13. Create a nested list (a list inside a list) and access an item inside the inner list.
+list1 = [2, 4, 5, 16, 8, 20, [3, 2, 5, 9], 9]
+item = list1[6][2]
+print(item)
 
+#14. Check if a list is empty.
+l = []
 
+if not l:
+    print("the list is empty")
+else:
+    print("the list is not empty")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#15. Convert a list of characters ['P', 'y', 't', 'h', 'o', 'n'] into a single string.
+chara = ['Python']
+single_string = "".join(chara)
+print(single_string)
 
 
 
