4b0261fb4c5722722cb955f4dbf17ea6de559a5b
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-26 20:22:39 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,138 +1,317 @@
-#Dictionary & Set (50 Questions)
-#Dictionary Basics (Q151–165)
-#1. Create a dictionary representing a Student with keys name, age, and grade.
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
+#Advanced Dictionary Logic (Q166–180)
+#1. Create a nested dictionary representing a classroom of 3 students.
+'''nes_dic = {
+    "student 1" : {
+        "name" : "anamika",
+        "age" : 12, 
+        "grade" : "A"
+    },
+    "student 2" : {
+        "name" : "aayushi",
+        "age" : 13, 
+        "grade" : "C"
+    },
+    "student 3" : {
+        "name" : "shreya", 
+        "age" : 14,
+        "grade" : "B"
+    }
 }
-print(student
+print(nes_dic)
 
-#2. Access and print the name of the student from the dictionary.
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
+#2. Access the grade of the second student inside the nested dictionary.
+classroom = {
+    "student 1" : {
+        "name" : "anamika",
+        "age" : 12, 
+        "grade" : "A"
+    },
+    "student 2" : {
+        "name" : "aayushi",
+        "age" : 13, 
+        "grade" : "C"
+    },
+    "student 3" : {
+        "name" : "shreya", 
+        "age" : 14,
+        "grade" : "B"
+    }
 }
-print(student["name"])
+
+print(classroom['student 2']['name'])
 
 #3. Use the .get() method to safely retrieve the grade key without throwing an error.
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
+classroom = {
+    "student 1" : {
+        "name" : "anamika",
+        "age" : 12, 
+        "grade" : "A"
+    },
+    "student 2" : {
+        "name" : "aayushi",
+        "age" : 13, 
+        "grade" : "C"
+    },
+    "student 3" : {
+        "name" : "shreya", 
+        "age" : 14,
+        "grade" : "B"
+    }
 }
-print(student.get("grade"))
+grade = classroom.get('student 2', {}).get('grade', 'Grade not found')
+print(grade
 
-#4. Add a new key-value pair school: "MNC High" to your dictionary.
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
+#4. Map two lists (one of names, one of scores) into a single dictionary.
+names = ['anamika', 'aayushi', 'shreya']
+scores = [94, 74, 86]
+
+classroom_scores = dict(zip(names, scores))
+print(classroom_scores)
+
+#5. Create a dictionary where the keys are numbers 1 to 5, and values are their squares.
+num = {
+    1 : 1**2,
+    2 : 2**2,
+    3 : 3**2,
+    4 : 4**2,
+    5 : 5**2
 }
-student["school"] = "MNC High"
-print(student)
+print(num)
 
-#5. Update the value of the age key in the student dictionary.
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
-}
-student["age"] = 87
-print(student)
-
-#6. Remove a key-value pair from the dictionary using del.
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
-}
-del student["age"]
-print(student)
-
-#7. Remove a key-value pair and return its value using .pop().
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
-}
-removed_age = student.pop("age")
-print("updated Dictionary: ", student)
-print("Removed Value: ",removed_age )
-
-#8. Print all the keys of a dictionary using .keys().
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
-}
-print(student.keys())
-
-#9. Print all the values of a dictionary using .values().
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
-}
-print(student.values())
-
-#10. Print all key-value pairs as a list of tuples using .items().
-student = {
-    "name" :"Anamika",
-    "age" :12,
-    "grade" :"A"
-}
-pairs_list = list(student.items())
-print(pairs_list)
-
-#11. Check if the key "salary" exists inside an employee dictionary.
-employee_dict = {
-    "name": "Anamika",
-    "salary": "30000", 
-    "working_days": "28"
-}
-if "salary" in employee_dict:
-    print("salary exits")
-else:
-    print("salary not exits")
-
-#12. Find the total number of key-value pairs in a dictionary.
-employee_dict = {
-    "name": "Anamika",
-    "salary": "30000", 
-    "working_days": "28"
-}
-print(len(employee_dict))
-
-#13. Clear all items from a dictionary to make it empty.
-employee_dict = {
-    "name": "Anamika",
-    "salary": "30000", 
-    "working_days": "28"
-}
-print(employee_dict.clear())
-print(employee_dict)
-
-#14. Create a dictionary with integer keys and print their values.
-int_dict = {
-    1 : "anamika",
-    2 : "shreya", 
-    3 : "aayushi"
-}
-print(int_dict)
-
-#15. Merge two separate dictionaries dict1 and dict2 together.
-int_dict = {
-    1 : "anamika",
-    2 : "shreya", 
-    3 : "aayushi"
+#6. Remove all keys with None values from a dictionary.
+num = {
+    1 : 1**2,
+    2 : 2**2,
+    3 : 3**2,
+    4 : 4**2,
+    5 : 5**2
 }
 
-employee_dict = {
-    "name": "Anamika",
-    "salary": "30000", 
-    "working_days": "28"
+s = num.clear()
+print(s)
+print(num)
+
+"""
+7. Extract a subset of a dictionary (keys a and b only) into a new dictionary.
+original_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+keys_to_extract = {'a', 'b'}
+subset_dict = {key: original_dict[key] for key in keys_to_extract if key in original_dict}
+print(subset_dict)
+"""
+
+#8. Find the key with the highest value in a dictionary.
+num_dict = {
+    'num1' : 24,
+    'num2' : 45,
+    'num3' : 73,
+    'num4' : 12
 }
-sum = int_dict | employee_dict
-print(sum)
+highest_value = max(num_dict, key=num_dict.get)
+print(highest_value)
+
+#9. Check if all values in a dictionary are identical.
+num_dict = {
+    'num1' : 10,
+    'num2' : 10,
+    'num3' : 10,
+    'num4' : 10
+}
+
+all_same_1 = len(set(num_dict.values())) == 1
+print(f"Are all values in num_dict identical? {all_same_1}")
+
+#10. Reverse a dictionary (swap keys and values, assuming values are unique).
+num_dict = {
+    'num1' : 22,
+    'num2' : 11,
+    'num3' : 70,
+    'num4' : 19
+}
+reversed_dict = {value: key for key, value in num_dict.items()}
+print(reversed_dict)
+
+#11. Create a dictionary of words and their lengths from a list of words.
+lis = ["apple", "banana", "mango", "kiwi"]
+word_len = {word: len(word) for word in lis}
+print(word_len)
+
+#12. Use a dictionary to map weekdays (1 -> "Monday", 2 -> "Tuesday", etc.).
+week_dict = {
+    1 : "monday",
+    2 : "Tuesday", 
+    3 : "wednesday", 
+    4 : "thursday", 
+    5 : "friday",
+    6: "Saturday",
+    7: "Sunday"
+}
+
+print("weekdays 3:", week_dict[3])
+print("weekdays 7:", week_dict.get(7, "Invalid day"))
+print(week_dict) 
+
+#13. Sort a dictionary by its keys in alphabetical order.
+dict_to_sort = {
+    'banana': 3,
+    'apple': 4,
+    'pear': 1,
+    'orange': 2
+}
+
+sorted_dict = dict(sorted(dict_to_sort.items()))
+print(sorted_dict)'''
+
+#14. Sort a dictionary by its values in ascending order.
+dict_to_sort = {
+    'banana': 3,
+    'apple': 4,
+    'pear': 1,
+    'orange': 2
+}
+
+sorted_dict_by_values = dict(sorted(dict_to_sort.items(), key=lambda item: item[1]))
+print(sorted_dict_by_values)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
