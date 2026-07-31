0496b33033ba5063686fc17375115a6a2a94541b
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-07-31 20:04:53 +0530

    update changes

--- practice_questions.py
+++ practice_questions.py
@@ -1,165 +1,100 @@
-#Sets & Set Operations (Q181–200)
-#1. Create a set of 5 prime numbers and print it.
-set1 = {1, 5, 3, 7, 9}
-print(set1)
+#Topic 5: Loops in Python (50 Questions)
+#For Loops (Q201–215)
+#1. Print numbers from $1$ to $10$ using a for loop.
 
-#2. Try adding a duplicate number to a set and observe that duplicates are ignored.
-set1 = {2, 6, 3, 2}
-print(set1)
-
-#3. Add a new element to a set using .add().
-set1 = {2, 6, 3, 2}
-set1.add(4)
-print(set1)
-
-#4. Remove an element from a set using .remove() (raises error if element not found).
-set1 = {2, 6, 3, 7}
-set1.remove(3)
-print(set1)
-
-#5. Safely remove an element from a set using .discard() (does not raise error).
-set1 = {2, 6, 3, 7}
-set1.discard(3)
-print(set1)
-
-#6. Find the union of two sets of numbers.
-
-set1 = {2, 6, 3, 7}
-set2 = {3, 5, 8, 7}
-result = set1.union(set2)
-print(result)
-
-#7. Find the intersection of two sets of numbers.
-set1 = {2, 6, 3, 7}
-set2 = {3, 5, 8, 7}
-result = set1.intersection(set2)
-print(result)
-
-#8. Find the difference between two sets (elements in A but not in B).
-a = {2, 6, 3, 7}
-b = {3, 5, 8, 7}
-result = a-b
-print(result)
-
-#9. Find the symmetric difference between two sets.
-a = {2, 6, 3, 7}
-b = {3, 5, 8, 7}
-result = a.symmetric_difference(b)
-print(result)
-
-#10. Check if a set is a subset of another set.
-a = {2, 6}
-b = {1, 2, 6, 7}
-print(a.issubset(b))
-
-#11. Check if two sets have no elements in common (disjoint).
-a = {12, 4}
-b = {1, 2, 6, 7}
-print(a.isdisjoint(b))
-
-#12. Convert a list of duplicate numbers into a set to find unique values.
-list1 = (2, 5, 6, 8, 2, 3, 6, 5)
-result = set(list1)
-print(result)
-
-#13. Find the length of a set.
-b = {1, 2, 6, 7}
-print(len(b))
-
-#14. Clear all elements from a set.
-b = {1, 2, 6, 7}
-result = b.clear()
-print(result)
-
-#15. Remove and return an arbitrary element from a set using .pop().
-b = {1, 2, 6, 7}
-result = b.pop()
-print(result)
-print(b)
-
-#16.Convert a set into a sorted list.
-b = {8, 3, 5, 7}
-result = sorted(b)
-print(result)
-
-#17. Check if an item exists inside a set.
-b = {1, 2, 6, 7}
-if 6 in b:
-    print('element exits in set b')
-else:
-    print("element not exits")
-
-#18. Update a set with elements from a list.
-b = {1, 2, 6, 7}
-list1 = [5, 8, 3]
-b.update(list1)
-print(b)
-
-#19. Write a program to find common elements from three different sets.
-a = {2, 5, 3, 7}
-b = {3, 5, 8, 7}
-c = {2, 3, 7, 5}
-result = a.intersection(b,c)
-print(result)
+'''for i in range(1,11,1):
+    print(i)
 
 
-#20. Create a set of vowels and find how many vowels are present in a user's name.
-vowels = {"a", "e", "i", "o" "u"}
-name = input("enter user's name:").lower()
+#2. Print all even numbers between $1$ and $30$ using a for loop with a step.
+
+for i in range(2,31,2):
+    print(i)
+
+#3. Print all odd numbers between $1$ and $30$ using a for loop.
+for i in range(1, 31, 2):
+    print(i)
+
+#4. Sum all numbers from $1$ to $50$ using a loop and print the total.
+total = 0
+
+for i in range(1, 51):
+    total+=i
+print(total)
+
+#5. Print the multiplication table of a user-inputted number.
+num = int(input("enter a number:"))
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+#6. Print each character of the string "DEVELOPER" on a separate line.
+word = "DEVELOPER"
+
+for ch in word:
+    print(ch)
+
+#7.iterate over a list of names and print a greeting for each.
+names = ["vanshika","misha","ishqi","mihir"]
+
+for name in names:
+    print(f"Hello: , {name}! Hope you have a great day.")
+
+#8. Print numbers from $10$ down to $1$ using a for loop and range().
+
+for i in range(10,0,-1):
+    print(i)
+
+#9. Print the squares of all numbers from $1$ to $10$.
+
+for i in range(1,11,1):
+    product  = i**2
+    print(product)
+
+#10. Count the total number of vowels in a string using a loop.
+string = input("Enter a word: ").lower()
+
 count = 0
 
-for letter in name:
-    if letter in vowels:
-        count += 1
+for ch in string:
+    if ch in "aeiou":
+        count+=1
+print("Total numbers:", count)
 
-print("number of vowels:", count)
+#11. Calculate the product of all numbers in a list.
+nums = [1, 5, 3, 6]
 
-b = {3, 5, 2}
-print(b)
+product = 1
 
+for num in nums:
+    product *=num
+    print(f"product of all numbers: {product}")
 
+#12. print a horizontal row of 10 asterisks (*) using a loop
 
+for i in range(10):
+    print("*", end= " ")
 
+#13. Print elements of a list along with their index using enumerate().
 
+nums = [3,2,5,8,7,1]
 
+for index, item in enumerate(nums):
+    print(index, item)
 
+#14. Print all numbers between $100$ and $200$ that are divisible by $7$.
 
+for i in range(100, 200):
+    if i%7 == 0:
+        print(i)'''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#15. Print the elements of a tuple in reverse order using a loop.
+    
+nums = (3, 5 ,8, 2, 9)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(len(nums) - 1, -1, -1):
+    print(nums[i])
 
 
 
