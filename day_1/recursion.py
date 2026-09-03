7b8b0f2730b2829af665eacef158b424eba06743
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-09-03 19:26:46 +0530

    update changes

--- day_1/recursion.py
+++ day_1/recursion.py
@@ -179,14 +179,19 @@ print(reuslt)
 from functools import reduce
 numbers=[5,10,15,20]
 result=reduce(lambda a,b:a+b, numbers)
-print(result)'''
+print(result)
 
 #Question 4-use filter() to keep only numbers divisible by 3 and greater than 10. use map() to multiply those numbers by 2. 
 
 numbres=[3,6,9,12,15,18,21]
 result=list(map(lambda n:n*2, filter(lambda n: n%3==0 and n>10, numbres)))
+print(result)'''
+
+#Question 5-
+from functools import reduce
+numbers=[1,2,3,4,5,6,7,8]
+result=reduce(lambda a,b:a+b, map(lambda n:n**2, filter(lambda n:n%2==0, numbers)))
 print(result)
-    
 
 
 
