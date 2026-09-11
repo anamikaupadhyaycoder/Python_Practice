4ea36ef363bc453eb82beea39c15d072d1344df9
Anamika Upadhyay <anamikaupadhyay216@gmail.com>
2026-09-11 15:31:55 +0530

    update changes

--- string.py
+++ string.py
@@ -1,18 +1,30 @@
 #starting with string 
 #1. 
-'''text="PROGRAMMING"
-print(text[0])
-print(text[-1])
-print(text[4])
-print(text[-3:])
-print(text[0:5])
-print(text[::-1])
-print(text[::2])
-
+'''def analyze_word(text):
+    s=text.startswith("Py")
+    r=text.endswith("ing")
+    e=text.count("o")
+    return s, r, e
+result=analyze_word("Python programming")
+print(result)
 #2. 
-text="python programming"
-print(text.find("java"))'''
+def analyze_word(text):
+    spaces=text.strip()
+    lower=text.lower()
+    count=len(text.split())
+    return spaces, lower, count
+result=analyze_word("   Python is AMAZING   ")
+print(result)'''
 
-#3.
-text="  "
-print(text.isspace())
+#3. 
+def extract_digits(text):
+    result= ""
+
+    for char in text:
+        if char.isdigit():
+            result+=char
+    return result
+
+r=extract_digits("Python123abc45")
+print(r)
+
